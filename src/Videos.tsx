019f6f6a2954add756e12c772115b3eefd582307
Josh Richards <joshuarichards001@gmail.com>
2025-01-16 20:00:43 +1300

    remove video

--- src/Videos.tsx
+++ src/Videos.tsx
@@ -1,5 +1,4 @@
 import useStore from "./store";
-import Video from "./Video";
 
 type Props = {
   playlistName: string;
@@ -15,7 +14,26 @@ export default function Videos({ playlistName }: Props) {
           <h2 className="font-bold text-xl mb-4">{playlistName}</h2>
           <ul className="flex flex-col">
             {videos.map((video) => (
-              <Video video={video} key={video.id} />
+              <li
+                className="flex flex-row cursor-move hover:bg-base-200 p-2 rounded-lg justify-between items-center w-full"
+                key={video.id}
+                draggable
+              >
+                <div className="flex flex-row">
+                  <img
+                    className="rounded-md h-20"
+                    src={video.snippet.thumbnails.default.url}
+                    alt={video.snippet.title}
+                  />
+                  <div className="flex flex-col pl-2 gap-2">
+                    <p>{video.snippet.title}</p>
+                    <p className="text-xs text-base-content/70">
+                      {video.snippet.videoOwnerChannelTitle}
+                    </p>
+                  </div>
+                </div>
+                <input type="checkbox" className="checkbox checkbox-lg mr-6" />
+              </li>
             ))}
           </ul>
         </div>
