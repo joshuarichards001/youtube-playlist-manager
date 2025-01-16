8fd78f7e67294b4b4155b6073508be251a2dcb72
Josh Richards <joshuarichards001@gmail.com>
2025-01-17 09:34:48 +1300

    Format main and add css module to types

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,5 @@
+declare module "*.css";
+
 type Playlist = {
   id: string;
   title: string;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = '246964970891-05k3i3p0q2a97u70mla8gu854q5r624u.apps.googleusercontent.com';
+const clientId =
+  "246964970891-05k3i3p0q2a97u70mla8gu854q5r624u.apps.googleusercontent.com";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-
-    <App />
+      <App />
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
